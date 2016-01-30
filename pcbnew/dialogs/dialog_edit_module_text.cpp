e9797ff041baa75573b0b8f6bf2480ec6c9d07e3
Carsten Schoenert <c.schoenert@t-online.de>
2016-01-30 17:18:57 +0100

    Imported Upstream version 0.20140224+bzr4027

--- pcbnew/dialogs/dialog_edit_module_text.cpp
+++ pcbnew/dialogs/dialog_edit_module_text.cpp
@@ -151,11 +151,13 @@ void DialogEditModuleText::OnOkClick( wxCommandEvent& event )
     if ( m_module)
         m_parent->SaveCopyInUndoList( m_module, UR_CHANGED );
 
+#ifndef USE_WX_OVERLAY
     if( m_dc )     //Erase old text on screen
     {
         m_currentText->Draw( m_parent->GetCanvas(), m_dc, GR_XOR,
                              (m_currentText->IsMoving()) ? MoveVector : wxPoint( 0, 0 ) );
     }
+#endif
     m_currentText->m_Text = m_Name->GetValue();
 
     m_currentText->m_Italic = m_Style->GetSelection() == 1 ? true : false;
@@ -206,11 +208,15 @@ void DialogEditModuleText::OnOkClick( wxCommandEvent& event )
 
     m_currentText->SetDrawCoord();
 
+#ifndef USE_WX_OVERLAY
     if( m_dc )     // Display new text
     {
         m_currentText->Draw( m_parent->GetCanvas(), m_dc, GR_XOR,
                              (m_currentText->IsMoving()) ? MoveVector : wxPoint( 0, 0 ) );
     }
+#else
+    m_parent->Refresh();
+#endif
 
     m_parent->OnModify();
 
